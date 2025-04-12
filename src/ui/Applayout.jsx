1b6919b91e7f4deb16bc6939b1e45d79e4e80789
theFarzin-m <frzin.af@gmail.com>
2025-04-12 09:37:39 +0330

    Remove reactlens

--- src/ui/Applayout.jsx
+++ src/ui/Applayout.jsx
@@ -1,13 +1,10 @@
 import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
-import { ReactLenis } from "lenis/react";
 
 import NavbarComponent from "./NavbarComponent";
 import Footer from "./Footer";
 import LoadingScreen from "./LoadingScreen";
 import Collapse from "../features/collapse/Collapse";
-import { useSelector } from "react-redux";
-import OnPageChange from "./OnPageChange";
 
 export default function Applayout() {
   const [isLoading, setISLoading] = useState(true);
@@ -17,7 +14,6 @@ export default function Applayout() {
   if (isLoading) return <LoadingScreen />;
 
   return (
-    <ReactLenis root>
       <div className="d-flex flex-nowrap">
           <div id="collapse-main" className="d-block d-lg-none">
             <Collapse />
@@ -30,6 +26,5 @@ export default function Applayout() {
           <Footer />
         </div>
       </div>
-    </ReactLenis>
   );
 }
